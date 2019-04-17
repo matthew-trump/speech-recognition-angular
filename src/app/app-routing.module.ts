2a45f22d2568cee9fc89adbc568ac13d3885d7b6
matthew-trump <matthew.trump@gmail.com>
2019-04-17 10:54:57 -0700

    route for google speech demo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoogleSpeechComponent } from './google-speech/google-speech.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'google-speech', pathMatch: 'full', component: GoogleSpeechComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
